915b820c2b8d835bcf173913155ee2deafa681b7
DaoNgocHaiAnh <anhdnhph26652@fpt.edy.vn>
2022-11-23 23:34:38 +0700

    hoa don

---
+++ app/src/main/java/com/example/pro1121_cp17312_nhom8_quanlycafe/Fragment/DisplayStatisticFragment.java
@@ -0,0 +1,69 @@
+package com.example.pro1121_cp17312_nhom8_quanlycafe.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.pro1121_cp17312_nhom8_quanlycafe.Activities.DetailStatisticActivity;
+import com.example.pro1121_cp17312_nhom8_quanlycafe.Adapter.AdapterDisplayStatistic;
+import com.example.pro1121_cp17312_nhom8_quanlycafe.DAO.DonDatDAO;
+import com.example.pro1121_cp17312_nhom8_quanlycafe.DTO.DonDatDTO;
+import com.example.pro1121_cp17312_nhom8_quanlycafe.MainActivity;
+import com.example.pro1121_cp17312_nhom8_quanlycafe.R;
+
+
+import java.util.List;
+
+public class DisplayStatisticFragment extends Fragment {
+
+    ListView lvStatistic;
+    List<DonDatDTO> donDatDTOS;
+    DonDatDAO donDatDAO;
+    AdapterDisplayStatistic adapterDisplayStatistic;
+    FragmentManager fragmentManager;
+    int madon, manv, maban;
+    String ngaydat, tongtien;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.displaystatistic_layout,container,false);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Quản lý thống kê");
+        setHasOptionsMenu(true);
+
+        lvStatistic = (ListView)view.findViewById(R.id.lvStatistic);
+        donDatDAO = new DonDatDAO(getActivity());
+
+        donDatDTOS = donDatDAO.LayDSDonDat();
+        adapterDisplayStatistic = new AdapterDisplayStatistic(getActivity(),R.layout.custom_layout_displaystatistic,donDatDTOS);
+        lvStatistic.setAdapter(adapterDisplayStatistic);
+        adapterDisplayStatistic.notifyDataSetChanged();
+
+        lvStatistic.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                madon = donDatDTOS.get(position).getMaDonDat();
+                manv = donDatDTOS.get(position).getMaNV();
+                maban = donDatDTOS.get(position).getMaBan();
+                ngaydat = donDatDTOS.get(position).getNgayDat();
+                tongtien = donDatDTOS.get(position).getTongTien();
+
+                Intent intent = new Intent(getActivity(), DetailStatisticActivity.class);
+                intent.putExtra("madon",madon);
+                intent.putExtra("manv",manv);
+                intent.putExtra("maban",maban);
+                intent.putExtra("ngaydat",ngaydat);
+                intent.putExtra("tongtien",tongtien);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+}
