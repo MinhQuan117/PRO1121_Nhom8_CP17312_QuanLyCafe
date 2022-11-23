bda5e88719dd082185026221bca33dc0b1b9dc8f
MinhQuan117 <minhquan1172003@gmail.com>
2022-11-23 20:05:18 +0700

    giao diện mới

--- app/src/main/java/com/example/pro1121_cp17312_nhom8_quanlycafe/Fragment/FragmentBan.java
+++ app/src/main/java/com/example/pro1121_cp17312_nhom8_quanlycafe/Fragment/FragmentBan.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +13,8 @@ import androidx.fragment.app.Fragment;
 import com.example.pro1121_cp17312_nhom8_quanlycafe.R;
 
 public class FragmentBan extends Fragment {
+    ImageView img_customtable_GoiMon;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,5 +24,15 @@ public class FragmentBan extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        img_customtable_GoiMon = view.findViewById(R.id.img_customtable_GoiMon);
+
+        img_customtable_GoiMon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentThucDon fragmentThucDon = new FragmentThucDon();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, fragmentThucDon, null).commit();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pro1121_cp17312_nhom8_quanlycafe/Fragment/FragmentTrangChu.java
+++ app/src/main/java/com/example/pro1121_cp17312_nhom8_quanlycafe/Fragment/FragmentTrangChu.java
@@ -4,14 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.pro1121_cp17312_nhom8_quanlycafe.R;
 
 public class FragmentTrangChu extends Fragment {
+    TextView txt_displayhome_ViewAllCategory, txt_displayhome_ViewAllStatistic;
 
     @Nullable
     @Override
@@ -22,5 +25,24 @@ public class FragmentTrangChu extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        txt_displayhome_ViewAllCategory = view.findViewById(R.id.txt_displayhome_ViewAllCategory);
+        txt_displayhome_ViewAllStatistic = view.findViewById(R.id.txt_displayhome_ViewAllStatistic);
+
+        txt_displayhome_ViewAllCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentThucDon fragmentThucDon = new FragmentThucDon();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, fragmentThucDon, null).commit();
+            }
+        });
+
+        txt_displayhome_ViewAllStatistic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentHoaDon fragmentHoaDon = new FragmentHoaDon();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, fragmentHoaDon, null).commit();
+            }
+        });
     }
 }
